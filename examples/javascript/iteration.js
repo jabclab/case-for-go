54dc08c23b8da76c657d366ee1fbced1bdf45212
Jake Clarkson <jacobwclarkson@gmail.com>
2016-08-14 21:46:00 +0100

    minor tweaks

--- examples/javascript/iteration.js
+++ examples/javascript/iteration.js
@@ -1,16 +1,17 @@
+'use strict';
+
 var arr = ['hello', 'tech', 'wednesday'];
 
 for (var i = 0, len = arr.length; i < len; i++) {
-    var element = arr[i];
-    // ...
+    console.log(arr[i]);
 }
 
-arr.forEach(function (element) { /* ... */ } );
+arr.forEach(function (element) { console.log(element); } );
 
-arr.forEach(element => { /* ... */ });
+arr.forEach(element => console.log(element));
 
-Array.prototype.forEach.call(arr, function (element) { /* ... */ });
+Array.prototype.forEach.call(arr, function (element) { console.log(element); });
 
-Array.prototype.forEach.call(arr, element => { /* ... */ });
+Array.prototype.forEach.call(arr, element => { console.log(element); });
 
-for (let element of arr) { /* ... */ }
+for (let element of arr) { console.log(element); }
